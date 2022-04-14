2b2c53623fe6a8605e1e2a6350d6c4a5fbd05bdb
Phil Mansfield <phil1@stanford.edu>
2022-04-14 13:58:33 -0700

    Added symlib setup files

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+version = "0.0.4"
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="symlib",
+    version=version,
+    author="Philip Mansfield",
+    author_email="mansfield.astro@gmail.com",
+    description="A library for working with data from the Symphony and MW-est zoom-in suites.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    url="https://github.com/phil-mansfield/symlib",
+    keywords=["python"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6"
+)
+#print(setuptools.find_packages(where="src"))
