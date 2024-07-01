bb1a38b38b81a59f5256c90e37b7bb9ba6069752
Phil Mansfield <mansfield.astro@gmail.com>
2024-07-01 09:39:37 -0700

    Changed copy_to_cardinal destination

--- docs/copy_to_cardinal.sh
+++ docs/copy_to_cardinal.sh
@@ -1,2 +1,5 @@
-scp index.html $cardinal:/afs/ir/group/gfc/WWW/symphony
-scp -r build/html $cardinal:/afs/ir/group/gfc/WWW/symphony/build
+#scp index.html $cardinal:/afs/ir/group/gfc/WWW/symphony
+#scp -r build/html $cardinal:/afs/ir/group/gfc/WWW/symphony/build
+
+scp index.html $cardinal:/afs/ir/group/gfc/WWW/gfcsims
+scp -r build/html $cardinal:/afs/ir/group/gfc/WWW/gfcsims/build
